2791ddf2568a661912b4d52199a241bcd8b7e4d4
Soran <soranahmadi.z.h@gmail.com>
2023-08-25 23:14:26 +0330

    Add project files.

---
+++ CountAnyBenchmark/CompareBenchMark.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountAnyBenchmark
+{
+    public class CompareBenchMark
+    {
+        private static readonly IEnumerable<int> _numbers = Enumerable.Range(1, 100000);
+        
+        [Benchmark]
+        public bool CheckAny()
+        {
+            return _numbers.Any();
+        }
+
+        [Benchmark]
+        public bool CheckCount()
+        {
+            return _numbers.Count()>0;
+        }
+
+        [Benchmark]
+        public bool CheckAnyWithCondition()
+        {
+            return _numbers.Any(n=>n%2000>0);
+        }
+
+        [Benchmark]
+        public bool CheckCountWithCondition()
+        {
+            return _numbers.Count(n => n % 2000 > 0) > 0;
+        }
+    }
+}
